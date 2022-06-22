0fe03b5314e29c9c43f5ec891b94bda8dbe0c6f9
Dimitar Dinkov <96382950+Mitkooo@users.noreply.github.com>
2022-06-22 13:38:44 +0300

    Add files via upload

---
+++ Kanbanize/Helpers/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Kanbanize.Helpers
+{
+    public class Helper
+    {
+        public static string BaseUrl = "https://testcorporation.kanbanize.com/index.php/api/kanbanize/";
+        public static string Login = "login/format/json";
+        public static string CreateTask = "/create_new_task/format/json";
+        public static string MoveTask = "move_task/format/json";
+        public static string DeleteTask = "delete_task/format/json";
+    }
+}

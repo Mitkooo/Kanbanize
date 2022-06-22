a5e356a39a6aeec8b7be579b707149519c8ab5a3
Dimitar Dinkov <96382950+Mitkooo@users.noreply.github.com>
2022-06-22 14:04:58 +0300

    Add files via upload

---
+++ Kanbanize/Kanbanize/DTOs/User/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Kanbanize.DTOs.User
+{
+    public class User
+    {
+        public string? Email { get; set; }
+        public string? Pass { get; set; }
+        public string? RealName { get; set; }
+        public string? CompanyName { get; set; }
+        public string? apiKey { get; set; }
+    }
+}
---
+++ Kanbanize/Kanbanize/Tests/Tests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using NUnit.Framework;
+using Kanbanize.KanbanizeRestClient;
+using System.Net.Http;
+using System.Net;
+
+namespace Kanbanize.Tests
+{
+    public class Tests : Setup
+    {
+        [Test]
+        public void ApiKeyIsReturned()
+        {
+            Assert.IsTrue(_apiKey != null);
+        }
+
+        [Test]
+        public void VerifyTaskhasBeenCreated()
+        {
+            _client = new Rest();
+            _task = _client.CreateTaskWithBoardId();
+            _response = _client.PostNewTask(_apiKey, _task);
+            Assert.That(_task.assignee == "Mitaka");
+            Assert.IsTrue(_response.StatusCode == HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void VerifyTaskHasBeenCreatedWithCorrectParameters()
+        {
+            _client = new Rest();
+            _task = _client.CreateTaskWithBoardId();
+            _response = _client.PostNewTask(_apiKey, _task);
+            Assert.That(_task.title == "Test task" && _task.description == "To do list");
+            Assert.That(_task.assignee == "Mitaka" && _task.priority == "High");
+            Assert.That(_task.column == "Requested" && _task.boardid == "1");
+            Assert.IsTrue(_response.StatusCode == HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void VerifyTaskHasBeenCreatedInCorrectColumn()
+        {
+            _client = new Rest();
+            _task = _client.CreateTaskWithBoardId();
+            _response = _client.PostNewTask(_apiKey, _task);
+            NUnit.Framework.Assert.That(_task.column == "Requested");
+            Assert.IsTrue(_response.StatusCode == HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void VerifyTaskHasBeenMoved()
+        {
+            _client = new Rest();
+            _task = _client.CreateTaskWithBoardIdAndTaskId();
+            _task.column = "Requested";
+            _response = _client.PostNewTask(_apiKey, _task);
+            var currentId = _response.Content.Split(':', StringSplitOptions.None).Last().Trim('}');
+            _task.taskid = currentId;
+            _response = _client.MoveTask(_apiKey, _task);
+            Assert.That(_task.column == "Requested");
+            Assert.IsTrue(_response.StatusCode == HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void VerifyTaskBasBeenDeleted()
+        {
+            _client = new Rest();
+            _task = _client.CreateTaskWithBoardIdAndTaskId();
+            _response = _client.PostNewTask(_apiKey, _task);
+            var currentId = _response.Content.Split(':', StringSplitOptions.None).Last().Trim('}');
+            _task.taskid = currentId;
+            _response = _client.DeleteTask(_apiKey, _task);
+            Assert.IsTrue(_response.StatusCode == HttpStatusCode.OK);
+        }
+    }
+}
